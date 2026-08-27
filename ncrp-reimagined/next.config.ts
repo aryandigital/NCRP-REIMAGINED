3c3c5c13d8babd669025b8b0e62f40e677cac550
Aryan Digital <aryan@raksha.dev>
2026-08-27 20:17:33 +0530

    chore: allow local network dev origins

--- ncrp-reimagined/next.config.ts
+++ ncrp-reimagined/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import os from "node:os";
+
+const lanOrigins = Object.values(os.networkInterfaces())
+  .flatMap((interfaces) => interfaces ?? [])
+  .filter((address) => address.family === "IPv4" && !address.internal)
+  .map((address) => address.address);
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["localhost", "127.0.0.1"],
+  allowedDevOrigins: ["localhost", "127.0.0.1", ...lanOrigins],
 };
 
 export default nextConfig;
